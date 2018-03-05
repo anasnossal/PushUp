55610ca818de70ea954b9177a843112c64804956
anastasianossal <an2832@tc.columbia.edu>
2018-03-04 22:56:51 -0500

    Modified MainActivity Added New File "TestAddFile.java"

--- app/src/main/java/com/example/android/pushup/MainActivity.java
+++ app/src/main/java/com/example/android/pushup/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     private void testUpload (){
+
         int i = 1+1;
+        int ii = 2+2;
+        int iii = i + ii;
     }
 }
---
+++ app/src/main/java/com/example/android/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.example.android.pushup;
+
+/**
+ * Created by anastasianossal on 3/4/18.
+ */
+
+public class TestAddFile {
+}
